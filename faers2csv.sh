44a62a6b03d4da2bc510f41f363d9e767792e149
dbuijs <dbuijs@gmail.com>
2016-02-11 10:33:20 -0500

    Update faers2csv.sh
    Removed whitespace in between keys in recs-tocsv commands.

--- faers2csv.sh
+++ faers2csv.sh
@@ -9,57 +9,57 @@ recs-annotate -k !^openfda! -MDigest::MD5=md5_hex \
   |tee >(recs-collate -k !^openfda! -a count \
   |recs-tocsv -k openfda_md5,\
   count,\
-  openfda_brand_name,\
-  openfda_generic_name,\
-  openfda_application_number,\
-  openfda_dosage_form,\
-  openfda_manufacturer_name,\
-  openfda_is_original_packager,\
-  openfda_product_ndc,\
-  openfda_nui,\
-  openfda_package_ndc,\
-  openfda_product_type,\
-  openfda_route,\
-  openfda_substance_name,\
-  openfda_spl_id,\
-  openfda_spl_set_id,\
-  openfda_pharm_class_epc,\
-  openfda_pharm_class_moa,\
-  openfda_pharm_class_cs,\
-  openfda_pharm_class_pe,\
-  openfda_upc,\
-  openfda_unii,\
-  openfda_rxcui |csvformat -B > csv/{/.}.openfda.csv) \
+openfda_brand_name,\
+openfda_generic_name,\
+openfda_application_number,\
+openfda_dosage_form,\
+openfda_manufacturer_name,\
+openfda_is_original_packager,\
+openfda_product_ndc,\
+openfda_nui,\
+openfda_package_ndc,\
+openfda_product_type,\
+openfda_route,\
+openfda_substance_name,\
+openfda_spl_id,\
+openfda_spl_set_id,\
+openfda_pharm_class_epc,\
+openfda_pharm_class_moa,\
+openfda_pharm_class_cs,\
+openfda_pharm_class_pe,\
+openfda_upc,\
+openfda_unii,\
+openfda_rxcui |csvformat -B > csv/{/.}.openfda.csv) \
   |recs-tocsv -k receiptdate,\
-  safetyreportid,\
-  actiondrug,\
-  activesubstancename,\
-  medicinalproduct,\
-  openfda_md5,\
-  openfda_brand_name,\
-  openfda_generic_name,\
-  drugadditional,\
-  drugcumulativedosagenumb,\
-  drugcumulativedosageunit,\
-  drugdosageform,\
-  drugintervaldosagedefinition,\
-  drugintervaldosageunitnumb,\
-  drugrecurreadministration,\
-  drugseparatedosagenumb,\
-  drugstructuredosagenumb,\
-  drugstructuredosageunit,\
-  drugadministrationroute,\
-  drugauthorizationnumb,\
-  drugbatchnumb,\
-  drugcharacterization,\
-  drugdosagetext,\
-  drugenddate,\
-  drugenddateformat,\
-  drugindication,\
-  drugstartdate,\
-  drugstartdateformat,\
-  drugtreatmentduration,\
-  drugtreatmentdurationunit |csvformat -B > csv/{/.}.drug.csv' ::: downloads/*.json.zip
+safetyreportid,\
+actiondrug,\
+activesubstancename,\
+medicinalproduct,\
+openfda_md5,\
+openfda_brand_name,\
+openfda_generic_name,\
+drugadditional,\
+drugcumulativedosagenumb,\
+drugcumulativedosageunit,\
+drugdosageform,\
+drugintervaldosagedefinition,\
+drugintervaldosageunitnumb,\
+drugrecurreadministration,\
+drugseparatedosagenumb,\
+drugstructuredosagenumb,\
+drugstructuredosageunit,\
+drugadministrationroute,\
+drugauthorizationnumb,\
+drugbatchnumb,\
+drugcharacterization,\
+drugdosagetext,\
+drugenddate,\
+drugenddateformat,\
+drugindication,\
+drugstartdate,\
+drugstartdateformat,\
+drugtreatmentduration,\
+drugtreatmentdurationunit |csvformat -B > csv/{/.}.drug.csv' ::: downloads/*.json.zip
 
 parallel --eta 'csvstat {} > stats/{/.}.report.txt' ::: csv/*.csv
 
